4e88594194b448a3e44742e9838158a59406d327
tews-code <thomas.ew.stuart@gmail.com>
2025-11-20 11:12:32 +0000

    Fixed merge conflict on run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -xue
 
-<<<<<<< HEAD
 #QEMU file path
 QEMU=qemu-system-riscv32
 
@@ -14,27 +13,6 @@ cp $1 kernel.elf
 
 #Start QEMU
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
-=======
-QEMU=qemu-system-riscv32
-CC=/opt/homebrew/opt/llvm/bin/clang
-OBJCOPY=/opt/homebrew/opt/llvm/bin/llvm-objcopy
-
-CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld -fno-stack-protector -ffreestanding -nostdlib"
-
-# Build the shell.
-$CC $CFLAGS -Wl,-Tuser.ld -Wl,-Map=shell.map -o shell.elf shell.c user.c common.c
-$OBJCOPY --set-section-flags .bss=alloc,contents -O binary shell.elf shell.bin
-$OBJCOPY -Ibinary -Oelf32-littleriscv shell.bin shell.bin.o
-
-# Build the kernel.
-$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
-    kernel.c common.c shell.bin.o
-
-(cd disk && tar cf ../disk.tar --format=ustar *.txt)
-
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
-    -d unimp,guest_errors,int,cpu_reset -D qemu.log \
->>>>>>> e519e74d33328c87f58e97558cd8477b396f8948
     -drive id=drive0,file=disk.tar,format=raw,if=none \
     -device virtio-blk-device,drive=drive0,bus=virtio-mmio-bus.0 \
     -kernel kernel.elf
